fdd86b7fb767aebd7bd9e64c528e5613bd68197a
Akos Kitta <kittaakos@typefox.io>
2020-04-14 14:13:27 +0200

    [drop-me]: Added an example.
    Signed-off-by: Akos Kitta <kittaakos@typefox.io>

--- examples/api-samples/src/browser/api-samples-frontend-module.ts
+++ examples/api-samples/src/browser/api-samples-frontend-module.ts
@@ -14,11 +14,46 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 
-import { ContainerModule } from 'inversify';
+import { ContainerModule, inject, injectable } from 'inversify';
 import { bindDynamicLabelProvider } from './label/sample-dynamic-label-provider-command-contribution';
 import { bindSampleUnclosableView } from './view/sample-unclosable-view-contribution';
+import { CommandRegistry, CommandContribution } from '@theia/core/';
+import { OutputChannelManager } from '@theia/output/lib/common/output-channel';
 
 export default new ContainerModule(bind => {
     bindDynamicLabelProvider(bind);
     bindSampleUnclosableView(bind);
+    bind(CommandContribution).to(SampleOutputChannelsCommandContribution).inSingletonScope();
 });
+
+@injectable()
+class SampleOutputChannelsCommandContribution implements CommandContribution {
+
+    @inject(OutputChannelManager)
+    private readonly ocm: OutputChannelManager;
+
+    private timers = new Map<string, number>();
+
+    registerCommands(commands: CommandRegistry): void {
+        for (const channelName of ['one', 'two', 'three']) {
+            const command = { id: `post-date-now-${channelName}`, label: `API Sample: Post Date.now() to the '${channelName}' channel.` };
+            commands.registerCommand(command, {
+                execute: () => {
+                    const timer = this.timers.get(channelName);
+                    if (timer === undefined) {
+                        this.timers.set(channelName, window.setInterval(() => {
+                            const channel = this.ocm.getChannel(channelName);
+                            if (channel) {
+                                channel.appendLine(`${channelName}: ${Date.now()}`);
+                            }
+                        }, 500));
+                    } else {
+                        window.clearInterval(timer);
+                        this.timers.delete(channelName);
+                    }
+                }
+            });
+        }
+    }
+
+}
